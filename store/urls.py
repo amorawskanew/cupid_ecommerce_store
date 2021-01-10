5e532a84234193f81e01363f5417f105b7eacca5
amorawskanew <amorawska@yahoo.com>
2021-01-10 03:46:03 +0000

    product page

--- store/urls.py
+++ store/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about/', views.aboutPage, name='about'),
+    path('product/', views.productPage, name='product'),
 
 ]
