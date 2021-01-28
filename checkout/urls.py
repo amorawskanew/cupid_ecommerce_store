afb93a3f3a7666f4a69d2e77ab31f1aef778c9b6
amorawskanew <amorawska@yahoo.com>
2021-01-28 03:11:02 +0000

    removing previous checkout.html page and replacing it with a new one

--- checkout/urls.py
+++ checkout/urls.py
@@ -5,5 +5,5 @@ from . import views
 # add checkout and Stripepayment oper url
 urlpatterns = [
     path('', views.checkout, name='checkout'),
-    path('stripepayment',views.stripeOpen,name="stripe_payment")
+    
 ]
--- checkout/views.py
+++ checkout/views.py
@@ -1,87 +1,116 @@
-from django.http.response import HttpResponse
-from django.shortcuts import redirect, render
-from django.conf import settings 
-from django.core.mail import send_mail 
-from  django.template.loader import render_to_string
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import  reverse
-from django.contrib import messages
 
-
-
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.conf import settings
 
 from .forms import OrderForm
+from .models import Order, OrderLineItem
+from products.models import Product
 from cart.contexts import cart_contents
 
 import stripe
 
 
-  
-from django.shortcuts import render, redirect, reverse
-from django.contrib import messages
-
-
-
-
 def checkout(request):
-    cart = request.session.get('cart', {})
-    if not cart:
-        messages.error(request, "There's nothing in your cart at the moment")
-        return redirect(reverse('products'))
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    order_form = OrderForm()
+    if request.method == 'POST':
+        cart = request.session.get('cart', {})
+
+        form_data = {
+            'full_name': request.POST['full_name'],
+            'email': request.POST['email'],
+            
+            'country': request.POST['country'],
+            'postcode': request.POST['postcode'],
+            'town_or_city': request.POST['town_or_city'],
+            'street_address1': request.POST['street_address1'],
+            'street_address2': request.POST['street_address2'],
+            'county': request.POST['county'],
+        }
+        order_form = OrderForm(form_data)
+        if order_form.is_valid():
+            order = order_form.save()
+            for item_id, item_data in cart.items():
+                try:
+                    product = Product.objects.get(id=item_id)
+                    if isinstance(item_data, int):
+                        order_line_item = OrderLineItem(
+                            order=order,
+                            product=product,
+                            quantity=item_data,
+                        )
+                        order_line_item.save()
+                    else:
+                        for size, quantity in item_data['items_by_size'].items():
+                            order_line_item = OrderLineItem(
+                                order=order,
+                                product=product,
+                                quantity=quantity,
+                                product_size=size,
+                            )
+                            order_line_item.save()
+                except Product.DoesNotExist:
+                    messages.error(request, (
+                        "One of the products in your cart wasn't found in our database. "
+                        "Please call us for assistance!")
+                    )
+                    order.delete()
+                    return redirect(reverse('view_cart'))
+
+            request.session['save_info'] = 'save-info' in request.POST
+            return redirect(reverse('checkout_success', args=[order.order_number]))
+        else:
+            messages.error(request, 'There was an error with your form. \
+                Please double check your information.')
+    else:
+        bag = request.session.get('cart', {})
+        if not bag:
+            messages.error(request, "There's nothing in your cart at the moment")
+            return redirect(reverse('products'))
+
+        current_cart = cart_contents(request)
+        total = current_cart['grand_total']
+        stripe_total = round(total * 100)
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
+
+        order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51IDRxKAxkkBXbzkdL03filqCsLzXXEp5RTqB072nycP4bcBlcKbpusYXl3CJyjeOioLIkswY9Jhyn2Q5aY3J1XYK009ButhPz5',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
 
-# Create your views here.
 
+def checkout_success(request, order_number):
+    """
+    Handle successful checkouts
+    """
+    save_info = request.session.get('save_info')
+    order = get_object_or_404(Order, order_number=order_number)
+    messages.success(request, f'Order successfully processed! \
+        Your order number is {order_number}. A confirmation \
+        email will be sent to {order.email}.')
 
-#add Checkout Function
-@login_required
-def checkout(request):
-    if request.method == 'POST':
-        return redirect('stripe_payment')
-    else:
-        return render(request, 'checkout/checkout.html')
+    if 'bag' in request.session:
+        del request.session['cart']
 
-# add stripe function
-@login_required
-def stripeOpen(request):
-    
-    if request.method == 'POST':
-        get_order = {
-                "order_number" : "123456",
-                "full_name":"anna",
-                "date": "22/01/2021",
-                "order_total":"order_total",
-                "delivery_cost":"delivery_cost",
-                "grand_total":"grand_total",
-                "street_address1":"street_address1",
-                "town_or_city":"town_or_city",
-                "country":"country",
-                
-            }
-            
-        body = render_to_string('confirmation_emails/email_body.html',{
-            'order' : get_order,
-            'contact_email' : 'contact_email'
-        })
-        email_subject = render_to_string('confirmation_emails/email_subject.html', {
-            'order_number':get_order.get('order_number')
-        })
-        email_from = settings.EMAIL_HOST_USER 
-        recipient_list = [request.POST.get('stripeEmail'), ] 
-        send_mail(subject=email_subject, message=body, from_email=email_from, recipient_list=recipient_list ) 
-        request.session['cart'] = {}
-        return redirect('home')
-    else:
-        return render(request, 'checkout/stripePayment.html')
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+    }
+
+    return render(request, template, context)
