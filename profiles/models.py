7d9d0bc43d2cbfa014e03ad36bd72e359a4b9950
amorawskanew <amorawska@yahoo.com>
2021-02-03 21:20:43 +0000

    email

--- profiles/models.py
+++ profiles/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-
 from django_countries.fields import CountryField
 
 
@@ -46,7 +45,5 @@ def create_or_update_user_profile(
 
     if created:
         UserProfile.objects.create(user=instance)
-
     # Existing users: just save the profile
-
     instance.userprofile.save()
