5088c5a6d8421a9fe50da9af781af14d1d4bd8cc
amorawskanew <amorawska@yahoo.com>
2021-02-04 00:16:30 +0000

    testing login/logout

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,7 @@
-from django.contrib import admin
+
 from django.urls import path
 from . import views
+
 urlpatterns = [
-    path('', views.index, name='home'),
-    path('logout',views.logoutWarning,name='account_logout'), #logout page url
-    path('userlogout',views.logoutUser,name="logoutuser") #logout user url
+    path('', views.index, name='home')
 ]
--- home/views.py
+++ home/views.py
@@ -1,22 +1,9 @@
-from django.shortcuts import redirect, render
-from django.contrib.auth import logout
 
+from django.shortcuts import render
+
+# Create your views here.
 
 def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
-
-
-# Redirect to logout page
-def logoutWarning(request):
-    if request.method == 'POST':
-        return redirect('account_login')
-    return render(request, 'allauth/account/logout.html')
-
-
-# Logout user function
-def logoutUser(request):
-    logout(request)
-    return redirect('account_login')
-
